b31eb99d4a40d586a18a766c007de86bca91381d
Jonathan Riddell <jr@jriddell.org>
2020-10-09 11:32:47 +0100

    asdf

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,19 @@
 
 set -ex
 
-$plant="giant-hogweed"
+plant="giant-hogweed"
 git checkout ${plant}
 git pull
 mkdir -p ../plantdentify.edinburghlinux.co.uk/${plant}
 ng build --prod --output-path=../plantdentify.edinburghlinux.co.uk/${plant} --base-href https://plantdentify.edinburghlinux.co.uk/${plant}/
 
-$plant="japanese-knotweed"
+plant="japanese-knotweed"
 git checkout ${plant}
 git pull
 mkdir -p ../plantdentify.edinburghlinux.co.uk/${plant}
 ng build --prod --output-path=../plantdentify.edinburghlinux.co.uk/${plant} --base-href https://plantdentify.edinburghlinux.co.uk/${plant}/
 
-$plant="himalayan-balsam"
+plant="himalayan-balsam"
 git checkout ${plant}
 git pull
 mkdir -p ../plantdentify.edinburghlinux.co.uk/${plant}
