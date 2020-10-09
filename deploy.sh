aeab66cde9edbd3f5d098fe31d51490f94418ad6
Jonathan Riddell <jr@jriddell.org>
2020-10-09 11:06:29 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,15 @@ set -ex
 
 git checkout hogweed
 git pull
+mkdir -p ../plantdentify.edinburghlinux.co.uk/hogweed
 ng build --prod --outputPath=../plantdentify.edinburghlinux.co.uk/hogweed
 
 git checkout foo
 git pull
+mkdir -p ../plantdentify.edinburghlinux.co.uk/foo
 ng build --prod --outputPath=../plantdentify.edinburghlinux.co.uk/foo
 
 git checkout bar
 git pull
+mkdir -p ../plantdentify.edinburghlinux.co.uk/bar
 ng build --prod --outputPath=../plantdentify.edinburghlinux.co.uk/bar
